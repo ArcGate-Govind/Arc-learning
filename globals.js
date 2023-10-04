a46ad1ecceee9971b89398b9d3840ff9940a6b66
varsha911 <varshapaliwal911@gmail.com>
2023-10-04 17:07:44 +0530

    add globals file

---
+++ globals.js
@@ -0,0 +1,2 @@
+export const API_URL =
+  "https://f3bf-2409-4052-2e08-a054-4d97-65a4-32e7-b086.ngrok-free.app/api/v1";
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 import { useRouter } from "next/navigation";
 import { removeUserSession, setUserSession } from "../utils/common";
-
+import { API_URL } from "../../globals";
 const ErroMessage = (message) => {
   return (
     <div className=" m-auto">
@@ -62,7 +62,7 @@ const Login = () => {
       let username = values.username;
       let password = values.password;
       await fetch(
-        "https://f3bf-2409-4052-2e08-a054-4d97-65a4-32e7-b086.ngrok-free.app/api/v1/login/",
+        `${API_URL}/login/`,
         {
           method: "POST",
           headers: {
