674d7d4736b0bf12e8411ec4b174f7997c2249e3
ArcGate-Govind <govind@arcgate.com>
2024-01-19 10:58:37 +0530

    add static url is dynmic

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { API_URL } from '../../constant';
 import { getAccessToken } from './common';
 export const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/v1/', 
+  baseURL: {API_URL} ,
   headers: {
     'Content-Type': 'application/json',
   },
